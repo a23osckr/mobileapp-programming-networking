0213cccbed21d08734baa53d077d7c3982857c14
AOkay12 <oscarkruit@live.se>
2024-04-22 14:22:40 +0200

    Added RecyclerViewAdapter.java along with methods and member variables

---
+++ app/src/main/java/com/example/networking/RecyclerViewAdapter.java
@@ -0,0 +1,59 @@
+package com.example.networking;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder> {
+
+    private ArrayList<Mountain> mountainArrayList;
+    private LayoutInflater layoutInflater;
+    private OnClickListener onClickListener;
+
+    RecyclerViewAdapter(Context context, ArrayList<Mountain> items, OnClickListener onClickListener) {
+        this.layoutInflater = LayoutInflater.from(context);
+        this.mountainArrayList = mountainArrayList;
+        this.onClickListener = onClickListener;
+    }
+
+    @Override
+    @NonNull
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return new ViewHolder(layoutInflater.inflate(R.layout.recycler_layout, parent, false));
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        holder.title.setText(mountainArrayList.get(position).getName());
+    }
+
+    @Override
+    public int getItemCount() {
+        return mountainArrayList.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+        TextView title;
+
+        ViewHolder(View itemView) {
+            super(itemView);
+            itemView.setOnClickListener(this);
+            title = itemView.findViewById(R.id.title);
+        }
+
+        @Override
+        public void onClick(View view) {
+            onClickListener.onClick(mountainArrayList.get(getAdapterPosition()));
+        }
+    }
+
+    public interface OnClickListener {
+        void onClick(Mountain mountain);
+    }
+}
